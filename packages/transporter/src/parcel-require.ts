3f3ed2f3fc156f417f6ed77e421251e54b91615c
Yanzhen Yu <yanzhen@smartx.com>
2020-12-25 15:23:55 +0800

    upgrade rrweb and housekeeping

--- packages/transporter/src/parcel-require.ts
+++
@@ -1,2 +0,0 @@
-// https://github.com/parcel-bundler/parcel/issues/1401
-((window as unknown) as { parcelRequire: number }).parcelRequire = 1;
--- packages/transporter/src/peerjs.ts
+++ packages/transporter/src/peerjs.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import './parcel-require';
 import Peer from 'peerjs';
 import {
   Transporter,
